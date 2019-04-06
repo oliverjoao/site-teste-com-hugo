f40161abc6c37d578ffbde53157e6029c79bab8d
João Cláudio <joao.claudio@outlook.com>
2019-04-06 19:01:31 -0300

    teste de novo

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -18,7 +18,7 @@ export default class PostPreview extends React.Component {
 
         <div className="bg-grey-1 pv4">
           <div className="flex-l mhn1-l ph3 center mw7">
-            <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}</h2>
+            <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading", "heading"])}</h2>
             <p className="w-60-l mb0">{entry.getIn(["data", "blurb", "text"])}</p>
           </div>
         </div>
